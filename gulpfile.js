4361255284c101423a52f445fed46bf7e3324d61
German Hernandez <muygrafico@gmail.com>
2017-11-08 10:23:19 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,47 @@
 var gulp = require('gulp'),
 sass = require('gulp-sass'),
 // nunjucksRender = require('gulp-nunjucks-render'),
-livereload = require('gulp-livereload');
+livereload = require('gulp-livereload'),
+connect = require('gulp-connect'),
+htmlLint = require('gulp-html-lint'),
+serve = require('gulp-serve');
 
 // gulp.task('default', function() { });
+
+gulp.task('serve', serve('site'));
+gulp.task('serve-build', serve(['public', 'build']));
+gulp.task('serve-prod', serve({
+  root: ['public', 'build'],
+  port: 80,
+  hostname: 'localhost',
+  middleware: function(req, res) {
+    // custom optional middleware
+  }
+}));
 
 gulp.task('sass', function () {
   gulp.src('site/styles/sass/**/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('site/styles/css'))
-  .pipe(livereload());
+  .pipe(livereload())
+  .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        livereload: true
+    });
+});
+
+
+
+gulp.task('html', function() {
+  gulp.src('site/*.html')
+  // .pipe(htmlLint())
+  // .pipe(htmlLint.format())
+  // .pipe(htmlLint.failOnError())
+  .pipe(livereload())
+  .pipe(connect.reload());
 });
 
 // gulp.task('nunjucks', function() {
@@ -33,6 +65,11 @@ gulp.task('watch', function() {
   // gulp.watch('less/*.less', ['less']);
   gulp.watch('site/sass/**/*.scss', ['sass']);
   gulp.watch('**/*.scss', ['sass']);
+  gulp.watch('site/*.html', ['html']);
   // gulp.watch('site/pages/**/*.+(html|nunjucks)', ['nunjucks']);
   // gulp.watch('site/**/*.*', ['nunjucks']);
 });
+
+gulp.task('default', ['serve','watch', 'connect']);
+
+livereload({ start: true })
